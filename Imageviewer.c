757b2fa58a17c27dda7a4b6a635de0d2cf16d96f
HarikaHari <hh453@nau.edu>
2016-12-06 21:48:37 -0700

    Add files via upload

--- Imageviewer.c
+++ Imageviewer.c
@@ -1,309 +1,381 @@
+#define GLFW_DLL 1
+
+#define GL_GLEXT_PROTOTYPES
+#include <GLES2/gl2.h>
 #include <GLFW/glfw3.h>
 
+#include "linmath.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#define IMAGE_MAX_SIZE 1024
+#include <assert.h>
 
 typedef struct pixel {
     float r, g, b;
 } pixel;
 
-typedef struct Image {
+/* typedef struct Image {
     int width, height;
     pixel* pixmap;
-} Image;
+} Image; */
+
+typedef struct PPM
+{
+    int width, height;
+    unsigned char *data;
+} PPM;
+
+// Create the default structure for the vertex 
+typedef struct
+{
+  float Position[2];
+  float TexCoord[2];
+} Vertex;
 
 
-
-
-int getNextWhiteSpace(FILE* fp, char buffer[], int buffer_size) {
-    int c;
-    int pos = 0;
-    while (1) {
-        if (pos > buffer_size - 1) {
-            fprintf(stderr, "Error: Buffer not large enough to finish reading to whitespace\n");
-            return -1;
-        }
-        c = getc(fp);
-        if (c == EOF) {
-            fprintf(stderr, "Error: Prematured End Of File");
-            return -1;
-        }
-        if(c == '\r'||c == '\n'||c == ' '||c == '\t') {
-            fseek(fp, -1, SEEK_CUR);
-            buffer[pos] = '\0';
-            return pos;
-        }
-        buffer[pos++] = (char)c;
-    }
-};
-
-
-
-int skipComments(FILE* fp) {
-    int temp;
-    char in = 0;
-    while (1) {
-        temp = getc(fp);
-        if (temp == EOF) {
-            fprintf(stderr, "Error: The file is not a valid PPM3 image file\n");
-            return 1;
-        }
-        if (in) {
-            if (temp == '\n' || temp == '\r')
-                in = 0;
-        }
-        else if (temp == '#')
-            in = 1;
-        else {
-            fseek(fp, -1, SEEK_CUR);
-            return 0;
-        }
-    }
-};
-
-int removeWhiteSpace(FILE* fp) {
-    int c;
-    do {
-        c = getc(fp);
-        // make sure we didn't read to the EOF
-        if (c == EOF) {
-            fprintf(stderr, "Error: Prematured End Of File");
-            return 1;
-        }
-        if (c == '\n' || c == '\r') {
-			if (skipComments(fp) != 0)
-			return 1;
-        }
-    }
-    while(c == '\r'||c == '\n'||c == ' '||c == '\t'); 
-    fseek(fp, -1, SEEK_CUR);
-    return 0;
-}
-
-
-int readP3(FILE* fp, Image* imagePtr, int MAX_COLOR, char buffer[]) {
-    int height = imagePtr->height;
-    int width = imagePtr->width;
-    imagePtr->pixmap = malloc(sizeof(pixel) * width * height);
-
-    // Read the actual image in
-    int i, j, k;
-    float value;
-    int bytes_read;
-    for (i=0; i<height; i++) {
-        for (j=0; j<width; j++) {
-            for (k=0; k<3; k++) {
-                // start of reading the pixel data into buffer
-                if (removeWhiteSpace(fp) != 0) {
-                    fprintf(stderr, "Error: An error occurred skipping file whitespace\n");
-                    return 1;
-                }
-
-                bytes_read = getNextWhiteSpace(fp, buffer, IMAGE_MAX_SIZE);
-                if (bytes_read <= 0) {
-                    fprintf(stderr, "Error: Expected a color value but read nothing\n");
-                    return 1;
-                }
-                else if (atoi(buffer) > MAX_COLOR) {
-                    fprintf(stderr, "Error: A color sample is greater than the maximum color (%i) value \n", MAX_COLOR);
-                    return 1;
-                }
-                else if (atoi(buffer) < 0) {
-                    fprintf(stderr, "Error: A negative color sample is not a valid value \n");
-                    return 1;
-                }
-                value = (atoi(buffer)/(float)MAX_COLOR);
-
-                if (k == 0)
-                    imagePtr->pixmap[i*width + j].r = value;
-                if (k == 1)
-                    imagePtr->pixmap[i*width + j].g = value;
-                if (k == 2)
-                    imagePtr->pixmap[i*width + j].b = value;
-            }
-        }
-    }
-	//end of reading the pixel data into buffer
-    return 0;
-}
-
-int readP6(FILE* fp, Image* imagePtr, int MAX_COLOR, char buffer[]) {
-    int height = imagePtr->height;
-    int width = imagePtr->width;
-    imagePtr->pixmap = malloc(sizeof(pixel) * width * height);
-
-    if (removeWhiteSpace(fp) != 0) {
-        fprintf(stderr, "Error: An error occurred skipping file whitespace\n");
-        return 1;
-    }
+PPM* readImage(char* name) {
+   	// Variables to Read the actual image into buffer
+    FILE *filePath;
+    char c;
+    int ppmType, width, height, maxColor;
+    int i, j, size, pixel;
 
     // start of reading the pixel data into buffer
-    int i, j, k;
-    size_t bytes_read;
-    float value;
-    for (i=0; i<height; i++) {
-        for (j=0; j<width; j++) {
-            for (k=0; k<3; k++) {
-                if (MAX_COLOR < 256) {
-                    bytes_read = fread(buffer, sizeof(char), 1, fp);
-                    if (bytes_read < 1) {
-                        fprintf(stderr, "Error: Expected a color value but read nothing\n");
-                        return 1;
-                    }
-                    value = buffer[0] & 0xFF;
-                }
-                else {
-                    bytes_read = fread(buffer, sizeof(char), 2, fp);
-                    if (bytes_read < 2) {
-                        fprintf(stderr, "Error: Expected a color value but read nothing\n");
-                        return 1;
-                    }
-                    value = ((buffer[0] << 8) & 0xFF00) | (buffer[1] & 0xFF);
-                }
+    PPM *tempImage = (PPM *)malloc(sizeof(PPM));
+	//opening the PPM file 
+    filePath = fopen (name, "r");
+    if(filePath == NULL)
+    {
+        fprintf(stderr, "\nERROR: Error in opening or reading file ");
+        fclose(filePath);
+        free(tempImage);
+        exit(-1);
+    }
+    fscanf(filePath, "P%c\n", &c);
+    ppmType = c -'0';
+    if (ppmType != 6 && ppmType != 3 ) 
+    {//if not in either p6 or p3 format then exit
+        fprintf(stderr, "\nERROR: This is not in the correct ppm format!");
+        free(tempImage);
+        fclose(filePath);
+        exit(-1);
+    }
+    c = getc(filePath);
+    //skip the comments since they do not matter
+    while(c =='#')
+    {
+        c = getc(filePath);
+        while(c!='\n') //read to the end of the line
+        {
+            c = getc(filePath);
+        }
+    }
+    ungetc(c, filePath);
+    fscanf(filePath, "%d %d %d\n", &width, &height, &maxColor);
+    if(maxColor > 255 || maxColor <= 0){
+        fprintf(stderr,"\nERROR: Image is not 8 bits per channel!");
+        fclose(filePath);
+        free(tempImage);
+        exit(-1);
+    }
+    size = width * height * 3;
+    if(!tempImage)
+    {
+        fprintf(stderr, "\nERROR: Error in memory allocation");
+        fclose(filePath);
+        free(tempImage);
+        exit(-1);
+    }
+    else{
+        tempImage->width = width;
+        tempImage->height = height;
+        tempImage->data = (unsigned char *)malloc(width*height*3);
+    }
 
-                value = (value/(float)MAX_COLOR);
+    if(!tempImage->data){
+        fprintf(stderr,"\nERROR:  Error in memory allocation for image pixels data");
+        fclose(filePath);
+        free(tempImage);
+        exit(-1);
+    }
 
-                if (k == 0)
-                    imagePtr->pixmap[i*width + j].r = value;
-                if (k == 1)
-                    imagePtr->pixmap[i*width + j].g = value;
-                if (k == 2)
-                    imagePtr->pixmap[i*width + j].b = value;
+    if(ppmType == 6)
+        fread((void *) tempImage->data, 1, (size_t) size, filePath);
+         
+    
+    else if(ppmType == 3)
+    {
+        for(i=0;i<height;i++)
+        {
+            for(j=0;j<width;j++)
+            {
+                fscanf(filePath, "%d ", &pixel);
+                tempImage->data[i*width*3+3*j] = pixel;
+                fscanf(filePath, "%d ", &pixel);
+                tempImage->data[i*width*3+3*j+1] = pixel;
+                fscanf(filePath, "%d ", &pixel);
+                tempImage->data[i*width*3+3*j+2] = pixel;
             }
         }
     }
 	//end of reading the pixel data into buffer
-    return 0;
+	fclose(filePath);
+	return tempImage;
 }
 
-int readImage(Image* imagePtr, char* name) {
-    FILE* fp = fopen(name, "r");
-    if (fp) {
-        int version = 0, width, height;
-        char buffer[IMAGE_MAX_SIZE];
-        int MAX_COLOR;
-
-        if (getNextWhiteSpace(fp, buffer, IMAGE_MAX_SIZE) != 2) {
-            fprintf(stderr, "ERROR: Invalid source file i.e. not a valid PPM file");
-            fclose(fp);
-            return 1;
-        }
-        if (strncmp("P3", buffer, 2) == 0) 
-            version = 3;
-        
-        else if (strncmp("P6", buffer, 2) == 0) 
-            version = 6;
-        
-        else {
-            fprintf(stderr, "ERROR: Invalid source file i.e. not a valid PPM file");
-            fclose(fp);
-            return 1;
-        }
-		
-        if (removeWhiteSpace(fp) != 0) {
-            fprintf(stderr, "Error: An error occurred skipping file whitespace\n");
-            fclose(fp);
-            return 1;
-        }
-
-        // width
-        if (getNextWhiteSpace(fp, buffer, IMAGE_MAX_SIZE) <= 0) {
-            fprintf(stderr, "Error: Expected a width value but read nothing\n");
-            return 1;
-        }
-
-        width = atoi(buffer);
-
-        if (width < 0)
-        {
-            fprintf(stderr, "ERROR: Invalid source file i.e. not a valid PPM file");
-            fclose(fp);
-            return 1;
-        }
-		printf("\n got the version %d",width);
- 
-        if (removeWhiteSpace(fp) != 0) {
-            fprintf(stderr, "Error: An error occurred skipping file whitespace\n");
-            fclose(fp);
-            return 1;
-        }
-
-        // height
-        if (getNextWhiteSpace(fp, buffer, IMAGE_MAX_SIZE) <= 0) {
-            fprintf(stderr, "Error: Expected a width value but read nothing\n");
-            return 1;
-        }
-
-        height = atoi(buffer);
-
-        if (height < 0)
-        {
-            fprintf(stderr, "ERROR: Invalid source file i.e. not a valid PPM file");
-            fclose(fp);
-            return 1;
-        }
 
 
-        if (removeWhiteSpace(fp) != 0) {
-            fprintf(stderr, "Error: An error occurred skipping file whitespace\n");
-            fclose(fp);
-            return 1;
-        }
+Vertex vertexes[] = {
+  {{1, -1}, {0.99999, 0.99999}},
+  {{1, 1},  {0.99999, 0}},
+  {{-1, 1}, {0, 0}},
+  {{-1, 1}, {0, 0}},
+  {{-1, -1}, {0, 0.99999}},
+  {{1, -1}, {0.99999, 0.99999}}
+};
 
-        // MAX_COLOR
-        if (getNextWhiteSpace(fp, buffer, IMAGE_MAX_SIZE) <= 0) {
-            fprintf(stderr, "Error: Expected a maximum color value but read nothing\n");
-            return 1;
-        }
 
-        MAX_COLOR = atoi(buffer);
+const double pi = 3.1415926535897;
+float rotation = 0;
+float scale = 1;
+float translate_x = 0;
+float translate_y = 0;
+float shear_x = 0;
+float shear_y = 0;
 
-        if (MAX_COLOR < 0 || MAX_COLOR > 65535)
-        {
-            fprintf(stderr, "Error: Expected maximum color value between 0 and 65536");
-            fclose(fp);
-            return 1;
-        }
 
-        imagePtr->width = (int) width;
-        imagePtr->height = (int) height;
-		printf("\n image pointer width %d",imagePtr->width);
-		printf("\n image pointer height %d",imagePtr->height);
-        int result;
-        if (version == 6)
-            result = readP6(fp, imagePtr, MAX_COLOR, buffer);
+static const char* vertex_shader_text =
+    "uniform mat4 MVP;\n"
+    "attribute vec2 TexCoordIn;\n"
+    "attribute vec2 vPos;\n"
+    "varying vec2 TexCoordOut;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+    "    TexCoordOut = TexCoordIn;\n"
+    "}\n";
 
-        if (version == 3)
-            result = readP3(fp, imagePtr, MAX_COLOR, buffer);
 
-        fclose(fp);
-        return result;
-    }
-    else {
-        fprintf(stderr, "Error: An error occurred while reading the image file.\n", name);
-        return 1;
+static const char* fragment_shader_text =
+    "varying lowp vec2 TexCoordOut;\n"
+    "uniform sampler2D Texture;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
+    "}\n";
+
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    // Hit escape to quite the ez-view program
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    // Rotate the image Left wise 90 degrees using W key
+    if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+    	rotation += 90*pi/180;
+
+    // Rotate the image Right wise 90 degrees using E key
+    if (key == GLFW_KEY_E && action == GLFW_PRESS)
+    	rotation -= 90*pi/180;
+
+    // Zoom into the image using =	key
+    if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS)//
+
+    	scale *= 2;
+
+    // Zoom out of the image using - key
+    if (key == GLFW_KEY_MINUS && action == GLFW_PRESS)
+    	scale *= .5;
+
+    // Translate the image down using down arrow
+    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    	translate_y += .1;
+
+    // Translate the image Up using up arrow
+    if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+    	translate_y -= .1;
+
+    // Translate the image left using left arrow
+    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    	translate_x += .1;
+
+    // Translate the image right using right arrow
+    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    	translate_x -= .1;
+
+    // Shear image upward using W key
+    if (key == GLFW_KEY_W && action == GLFW_PRESS) //Shear Up
+    	shear_y += .1;
+
+    // Shear image downward using S key
+    if (key == GLFW_KEY_S && action == GLFW_PRESS) //Shear Down
+    	shear_y -= .1;
+
+    // Shear image right using D key
+    if (key == GLFW_KEY_D && action == GLFW_PRESS) //Shear Right
+    	shear_x += .1;
+
+    // Shear image left using A key
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) //Shear Left
+    	shear_x -= .1;
+}
+
+
+void glCompileShaderOrDie(GLuint shader)
+{
+    GLint compiled;
+    glCompileShader(shader);
+    glGetShaderiv(shader,
+        GL_COMPILE_STATUS,
+        &compiled);
+
+    if (!compiled)
+    {
+        GLint infoLen = 0;
+        glGetShaderiv(shader,
+              GL_INFO_LOG_LENGTH,
+              &infoLen);
+        char* info = malloc(infoLen+1);
+        GLint done;
+        glGetShaderInfoLog(shader, infoLen, &done, info);
+        printf("Unable to compile shader: %s\n", info);
+        exit(-1);
     }
 }
+
+
 
 
 int main (int argc, char *argv[]) {
+
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location;		
     // Check input arguments
     if (argc != 2) {
-        fprintf(stderr, "Error: Not enough arguments provided\n");
+        fprintf(stderr, "Error: Please provide the file name as an argument\n");
         return 1;
-    }
-	Image image;
+    }	
     char *fileName = argv[1];
     // start of code to load ppm image file
-  	int loadTemp = readImage(&image, fileName);
-	
-    if ( loadTemp != 0) {
-        fprintf(stderr, "Error: An error occurred loading image file.\n");
+  	PPM *image = readImage(fileName);
+    if (image->width < 0) {
+        fprintf(stderr, "Error: Error in  loading image file.\n");
         exit(1);
+    }	
+	if (!glfwInit())
+        exit(EXIT_FAILURE);
+    // Same hints used in the demo provided
+	glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    window = glfwCreateWindow(640, 480, "Image Viewer", NULL, NULL);
+    // window initialization 
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
     }
-	 // end of code to load ppm image file
+    // Turn on key callback in order to take in the user inputs
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    // NOTE: OpenGL error checks have been omitted for brevity
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShaderOrDie(vertex_shader);
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShaderOrDie(fragment_shader);
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    mvp_location = glGetUniformLocation(program, "MVP");
+    assert(mvp_location != -1);
+    vpos_location = glGetAttribLocation(program, "vPos");
+    assert(vpos_location != -1);
+    GLint texcoord_location = glGetAttribLocation(program, "TexCoordIn");
+    assert(texcoord_location != -1);
+    GLint tex_location = glGetUniformLocation(program, "Texture");
+    assert(tex_location != -1);
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location,
+                          2,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          sizeof(Vertex),
+                          (void*) 0);
+
+    glEnableVertexAttribArray(texcoord_location);
+    glVertexAttribPointer(texcoord_location,
+                          2,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          sizeof(Vertex),
+                          (void*) (sizeof(float) * 2));
+
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 GL_RGB,
+                 image->width,
+                 image->height,
+                 0,
+                 GL_RGB,
+                 GL_UNSIGNED_BYTE,
+                 image->data);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glUniform1i(tex_location, 0);
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int windowWidth, windowHeight;
+        mat4x4 r, h, s, t, rh, rhs, mvp; 
+        glfwGetFramebufferSize(window, &windowWidth, &windowHeight);
+        ratio = windowWidth / (float) windowHeight;
+        glViewport(0, 0, windowWidth, windowHeight);
+        glClear(GL_COLOR_BUFFER_BIT);
+        mat4x4_identity(r);
+        mat4x4_rotate_Z(r, r, rotation); 
+        mat4x4_identity(h);
+        h[0][1] = shear_x;
+        h[1][0] = shear_y;
+        mat4x4_identity(s); 
+        s[0][0] = s[0][0]*scale;
+        s[1][1] = s[1][1]*scale;
+        mat4x4_identity(t);
+        mat4x4_translate(t, translate_x, translate_y, 0);
+        mat4x4_mul(rh, r, h); 
+        mat4x4_mul(rhs, rh, s);
+        mat4x4_mul(mvp, rhs, t);
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    free(image);
+    glfwDestroyWindow(window);
+    glfwTerminate();
     exit(EXIT_SUCCESS);
 }
